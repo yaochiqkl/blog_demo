30677b3c993ce73fcf83dde4ae11c6c3d8a34fd9
yaochiqkl <301100849@qq.com>
2016-02-01 11:56:20 +0800

    临时优化

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-//工具函数库
+//工具类函数库
 var utils = (function(){
-	var xmlHttp;
 	var commom_url = "http://fed.hz.netease.com/api/";
-	var createxmlHttpRequest = function(){
-		if(window.ActiveXObject){
-			xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");
-		}else if(window.XMLHttpRequest)
-			xmlHttp=new XMLHttpRequest();
-	};
 	var doGet = function(url) {
+		var xmlHttp;
+		var createxmlHttpRequest = function(){
+			if(window.ActiveXObject){
+				xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");
+			}else if(window.XMLHttpRequest)
+				xmlHttp=new XMLHttpRequest();
+		};
 		//createxmlHttpRequest();
-		var xmlHttp = new XMLHttpRequest();
+		xmlHttp = new XMLHttpRequest();
 		xmlHttp.open("GET",commom_url + url);
 		xmlHttp.send(null);
 		xmlHttp.onreadystatechange=function(){
@@ -37,27 +37,29 @@ var utils = (function(){
 	{
 		var d,s;
 		d = new Date();
-		s = 1900 + d.getYear() + "-";             //取年份
-		s = s + (d.getMonth() + 1) + "-";//取月份
-		s += d.getDate() + " ";         //取日期
-		s += d.getHours() + ":";       //取小时
-		s += d.getMinutes() + ":";    //取分
-		s += d.getSeconds();         //取秒
+		s = 1900 + d.getYear() + "-";            
+		s = s + (d.getMonth() + 1) + "-";
+		s += d.getDate() + " ";        
+		s += d.getHours() + ":";      
+		s += d.getMinutes() + ":";    
+		s += d.getSeconds();        
 		return(s);  
 	};
 	//直接插入排序
-	var insertSort = function(array){
+	var insertSort = function(array,param){
+		var x = param;
 		if (! (array instanceof Array)) {
-			console.log("Not Array!");
+			alert("Not Array!");
 			return;
 		}
 		var A = array;
 		var key = null;
 		var i = null;
+		//点表示法不支持通过变量来访问属性，故使用方括号表示法
 		for (var j = 1; j< A.length; j++) {
 			key = A[j];
 			i = j - 1;
-			while( i > -1 && A[i].publishTime < key.publishTime){
+			while( i > -1 && A[i][param] < key[param]){
 				A[i+1] = A[i];
 				i--;
 			}
@@ -74,6 +76,7 @@ var utils = (function(){
 })();
 //页面初始化函数库
 var pageInit = (function(){
+	//菜单栏、标签栏点击事件
 	var regBtn = function(){
 		//左侧菜单栏点击
 		var ls = document.querySelectorAll(".u-lst li");
@@ -107,12 +110,13 @@ var pageInit = (function(){
 			ls2[i].onclick = tabClick;  
 		}
 	};
+	//插入博客模块
 	var insertBlogs = function(){
 		//根据时间来排序
-		utils.sort(data);
+		utils.sort(data,"publishTime");
 		var html = '';
 		var piece = null;
-		//顶置日志
+		//顶置日志初始化
 		for( var i = 0; i < data.length; i++){
 			if( data[i].rank == 5 ){
 				data[i].private_id = i;
@@ -140,8 +144,8 @@ var pageInit = (function(){
 	            html += piece;
 			}
 		}
-		//普通日志
-		for( var i = 0; i < data.length; i++){
+		//普通日志初始化
+		for( i = 0; i < data.length; i++){
 			if( data[i].rank != 5 ){
 				data[i].private_id = i;
 				piece = '<div class="slt" data-id="'+i+'" >\
@@ -169,10 +173,11 @@ var pageInit = (function(){
 			}
 		}
         document.querySelector(".m-ctt .mn").innerHTML = html ;
-        //初始化单个的删除顶置事件
+        //初始化单个日志的删除和顶置事件
         blogsRegBtn();
         editRegBtn();
 	};
+	//单个日志的删除和顶置事件
 	var blogsRegBtn = function(){
 		//单个删除选项
 		var dlt = document.querySelectorAll(".m-ctt .dlt-single");
@@ -181,7 +186,7 @@ var pageInit = (function(){
 			var id = null;
 			//兼容低版本IE 取消使用 child.dataset.id
 			data.splice(child.getAttribute("data-id"),1);
-			pageInit.insertBlogs();
+			insertBlogs();
 		};
 		for (var i = 0; i < dlt.length; i++) {
 			dlt[i].onclick = dltClick;  
@@ -201,14 +206,14 @@ var pageInit = (function(){
 				child = this.parentNode.parentNode.parentNode.parentNode;
 				data[child.getAttribute("data-id")].rank = 0;
 			}
-			pageInit.insertBlogs();
+			insertBlogs();
 		};
 		for (var j = 0; j < top.length; j++) {
 			top[j].onclick = topClick;  
 		}
 	};
+	//全选、删除模块
 	var allRegBtn = function(){
-		//全选删除模块
 		document.querySelector(".all").onclick = function(){
 			var ls = document.querySelectorAll(".m-ctt .mn input[type=checkbox]");
 			if (this.checked){
@@ -233,11 +238,11 @@ var pageInit = (function(){
 			 	}
 			 }
 			}
-			pageInit.insertBlogs();
+			insertBlogs();
 		};
 	};
+	//日志发布模块
 	var pubRegBtn = function(){
-		//日志发布模块
 		var title = document.querySelector(".m-ipt .tt");
 		var textarea = document.querySelector(".m-ipt textarea");
 		var clear = document.querySelector(".m-ipt .clear");
@@ -293,15 +298,15 @@ var pageInit = (function(){
 					"shortPublishDateStr":""
 				};
 				data.push(new_item);
-				pageInit.insertBlogs();
+				insertBlogs();
 				title.value = "日志标题";
 				textarea.value = ""; 
 			}
 		};
 
 	};
+	//编辑选项初始化
 	var editRegBtn = function(){
-		//编辑选项
 		var edt = document.querySelectorAll(".m-ctt .edt");
 		var edtClick = function(){
 			var child = this.parentNode;
@@ -311,6 +316,20 @@ var pageInit = (function(){
 			edt[i].onclick = edtClick;  
 		}
 	};
+	//好友日志初始化
+	var insertFriendLog = function(){
+		var html = '';
+		for( var i = 0; i < 13 ; i++ ) {
+			html += '<div class="slt">\
+                        <img class="f-fl" src="img/avatar.jpg" alt="">\
+                        <div class="hd">小油菜</div>\
+                        <div class="bd">2hit:教你如何吧美妞拍的...</div>\
+                    </div>'
+		}
+		console.log(document.querySelector(".m-sd2 .content"));
+		document.querySelector(".m-sd2 .content").innerHTML = html;
+	};
+	//好友日志滚动模块
 	var scrollInit = function(lh,speed,delay){ 
 		var t = null; 
 		var o = document.querySelector(".m-sd2 .content");
@@ -370,7 +389,7 @@ var pageInit = (function(){
 			} else {
 				data[id].title = title.value;
 				data[id].blogContent = textarea.value;
-				pageInit.insertBlogs();
+				insertBlogs();
 				title.value = "日志标题";
 				textarea.value = ""; 
 				//重新绑定新日志的发布功能
@@ -378,17 +397,7 @@ var pageInit = (function(){
 			}
 		};
 	};
-	var insertFriendLog = function(){
-		var html = '';
-		for( var i = 0; i < 13 ; i++ ) {
-			html += '<div class="slt">\
-                        <img class="f-fl" src="img/avatar.jpg" alt="">\
-                        <div class="hd">小油菜</div>\
-                        <div class="bd">2hit:教你如何吧美妞拍的...</div>\
-                    </div>'
-		}
-		document.querySelector(".m-sd2 .content").innerHTML = html;
-	};
+	
 	return {
 		regBtn: regBtn,
 		insertBlogs: insertBlogs,
