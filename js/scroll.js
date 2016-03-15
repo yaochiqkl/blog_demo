d4492846820a392f8537ca36932f7a9337ab58c4
yaochiqkl <301100849@qq.com>
2016-03-15 17:23:02 +0800

    Added: Finish the homework of blog

--- js/scroll.js
+++ js/scroll.js
@@ -1,7 +1,8 @@
 /**
  * ------------------------------------------------------------------
- * 垂直滚动组件，能够实现循环滚动，滚动完延迟和悬停的功能。
- *
+ * 垂直滚动组件
+ * 实现循环滚动，滚动完延迟和悬停的功能
+ * 
  * @version  1.0
  * @date     2016-02-02
  * @author   lishuangzhuang<hzlishuangzhuang@corp.netease.com>
@@ -11,13 +12,13 @@ var scroll = (function(){
 	/**
 	 * 滚动组件初始化
 	 *
-	 * 具体描述一些细节
+	 * 首先会有delay的延迟，之后开始由speed决定的速度向上滚动lh的px
 	 *
 	 * @param    {string}  selector    需要滚动的目标元素节点
 	 * @param    {number}    lh        每次滚动的高度（单位：px）
 	 * @param    {number}   speed      速度，越小越快
 	 * @param    {number}   delay      滚动完的延迟（单位：ms）
-	 * @returns    void
+	 * @returns   void
 	 *
 	 */
 	var scrollInit = function(selector,lh,speed,delay){ 
